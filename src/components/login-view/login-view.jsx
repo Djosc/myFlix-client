26936b769f25a10c874cc18e31ab3d29e362ec94
Djosc <davidjoscaldwell@gmail.com>
2021-12-24 21:02:15 -0500

    Create bootstrap form for login-view + basic styling

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+import './login-view.scss';
 
 export function LoginView(props) {
 	const [username, setUsername] = useState('');
@@ -16,27 +19,25 @@ export function LoginView(props) {
 	};
 
 	return (
-		<form>
-			<label>
-				Username:
-				<input
-					type="text"
-					value={username}
-					onChange={(e) => setUsername(e.target.value)}
-				/>
-			</label>
-			<label>
-				Password:
-				<input
-					type="password"
-					value={password}
-					onChange={(e) => setPassword(e.target.value)}
-				/>
-			</label>
-			<button type="button" onClick={handleSubmit}>
-				Submit
-			</button>
-		</form>
+		<div className="login-form">
+			<Form>
+				<Form.Group controlId="formUsername">
+					<Form.Label>Username:</Form.Label>
+					<Form.Control type="text" onChange={(e) => setUsername(e.target.value)} />
+				</Form.Group>
+
+				<Form.Group controlId="formPassword">
+					<Form.Label>Password:</Form.Label>
+					<Form.Control
+						type="password"
+						onChange={(e) => setPassword(e.target.value)}
+					></Form.Control>
+				</Form.Group>
+				<Button variant="primary" type="submit" onClick={handleSubmit}>
+					Submit
+				</Button>
+			</Form>
+		</div>
 	);
 }
 
